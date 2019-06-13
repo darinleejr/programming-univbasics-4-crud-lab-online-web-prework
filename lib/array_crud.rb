fdd2729ed5bd1fc2373fa0746cb4df9b086bb753
Darin Webster <darinleenavy@gmail.com>
2019-06-13 18:35:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["dog", "cat", "fish", "bird"]
+  array << element
   
   add_element_to_end_of_array << "snake"
   #=> ["dog", "cat", "fish", "bird", "snake"]
